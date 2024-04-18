49b1798ef14eb2bfc47e10dfb4ba65c98084fa69
adelaf1999 <74821581+adelaf1999@users.noreply.github.com>
2024-04-18 16:54:35 -0400

    219 Extracting Form Logic

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://5b8b-69-171-156-4.ngrok-free.app'
+    baseURL: 'https://8dd3-142-188-73-77.ngrok-free.app'
 });
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -3,60 +3,23 @@ import {View, StyleSheet, TouchableOpacity} from "react-native";
 import { Text, Input, Button } from '@rneui/themed';
 import Spacer from "../components/Spacer";
 import {Context as AuthContext} from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
 
 const SignupScreen = ({navigation}) => {
 
     const {state, signup} = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
 
 
     return(
         <View style={styles.container}>
 
-            <Spacer>
-                <Text h3>Sign Up for Tracker</Text>
-            </Spacer>
-
-            <Spacer/>
-
-
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={(newEmail) => {
-                    setEmail(newEmail);
-                }}
-                autoCapitalize="none"
-                autoCorrect={false}
+            <AuthForm
+                headerText="Sign Up For Tracker"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign Up"
+                onSubmit={signup}
             />
 
-            <Spacer/>
-
-            <Input
-                secureTextEntry
-                label="Password"
-                value={password}
-                onChangeText={(newPassword) => {
-                    setPassword(newPassword);
-                }}
-                autoCapitalize="none"
-                autoCorrect={false}
-            />
-
-            {
-                state.errorMessage ?  <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null
-            }
-
-            <Spacer>
-                <Button
-                    title="Sign Up"
-                    onPress={() => {
-                        signup({email, password});
-                    }}
-                />
-            </Spacer>
 
             <TouchableOpacity
                 onPress={() => {
