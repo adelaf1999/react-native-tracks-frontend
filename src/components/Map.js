4a4ca462a5a1a534f298138ff47ef3243ef4157d
adelaf1999 <74821581+adelaf1999@users.noreply.github.com>
2024-04-27 15:38:41 -0400

    248 Drawing position indicator

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, {useContext} from "react";
 import {Text, StyleSheet, ActivityIndicator} from "react-native";
-import MapView, {Polyline} from 'react-native-maps';
+import MapView, {Polyline, Circle} from 'react-native-maps';
 import {Context as LocationContext} from "../context/LocationContext";
 
 const Map = () => {
@@ -20,6 +20,7 @@ const Map = () => {
         );
     }
 
+
     return (
 
         <MapView
@@ -29,12 +30,18 @@ const Map = () => {
                 latitudeDelta: 0.01,
                 longitudeDelta: 0.01,
             }}
-            region={{
-                ...currentLocation.coords,
-                latitudeDelta: 0.01,
-                longitudeDelta: 0.01,
-            }}
+            // region={{
+            //     ...currentLocation.coords,
+            //     latitudeDelta: 0.01,
+            //     longitudeDelta: 0.01,
+            // }}
         >
+            <Circle
+              center={currentLocation.coords}
+              radius={15}
+              strokeColor="rgba(158, 158, 255, 1.0)"
+              fillColor="rgba(158, 158, 255, 0.3)"
+            />
         </MapView>
 
     );
