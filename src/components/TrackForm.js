e42662c1dbe8640248fad1e6e5d0eabdb02e0a4a
adelaf1999 <74821581+adelaf1999@users.noreply.github.com>
2024-05-01 11:29:02 -0400

    254 Track Form Wire Up

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Input, Button } from '@rneui/themed';
 import Spacer from "./Spacer";
+import {Context as LocationContext} from "../context/LocationContext";
 
 const TrackForm = () => {
+
+    const {
+        state,
+        startRecording,
+        stopRecording,
+        changeName
+    } = useContext(LocationContext);
+
+    const {name, recording, locations} = state;
+
+    console.log(locations.length);
 
     return(
         <>
             <Spacer>
-                <Input placeholder="Enter Name"/>
+
+                <Input
+                    onChangeText={changeName}
+                    placeholder="Enter Name"
+                    value={name}
+                />
+
             </Spacer>
 
             <Spacer>
-                <Button title="Start Recording"/>
+
+                {
+                    recording ?
+                        <Button
+                            title="Stop"
+                            onPress={stopRecording}
+                        /> :
+                        <Button
+                            title="Start Recording"
+                            onPress={startRecording}
+                        />
+                }
+
+
             </Spacer>
+
 
         </>
     )
