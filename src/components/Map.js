3fa9b740fd2e038dd4db3922cf56f8bf2b62c07a
adelaf1999 <74821581+adelaf1999@users.noreply.github.com>
2024-05-02 11:03:57 -0400

    263 Bring Back the polyline

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,7 +7,7 @@ const Map = () => {
 
     const {state} = useContext(LocationContext);
 
-    const {currentLocation} = state;
+    const {currentLocation, locations} = state;
 
     if(!currentLocation){
         return (
@@ -38,10 +38,13 @@ const Map = () => {
         >
             <Circle
               center={currentLocation.coords}
-              radius={30}
+              radius={50}
               strokeColor="rgba(158, 158, 255, 1.0)"
               fillColor="rgba(158, 158, 255, 0.3)"
             />
+
+            <Polyline coordinates={locations.map(loc => loc.coords )} />
+
         </MapView>
 
     );
