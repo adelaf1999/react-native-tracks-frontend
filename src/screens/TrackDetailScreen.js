1de42cf68163dcf8caa39babcf0e736dd12516cc
adelaf1999 <74821581+adelaf1999@users.noreply.github.com>
2024-06-11 19:04:45 -0400

    274 Showing Track Details

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,6 +1,7 @@
 import React, {useContext} from "react";
 import {View, StyleSheet, Text} from "react-native";
 import {Context as TrackContext} from "../context/TrackContext";
+import MapView, {Polyline, Circle} from 'react-native-maps';
 
 const TrackDetailScreen = ({navigation, route}) => {
 
@@ -10,15 +11,40 @@ const TrackDetailScreen = ({navigation, route}) => {
 
     const track = state.find(t => t._id === _id);
 
+    const initialCoords = track.locations[0].coords;
+
 
     return(
-        <Text style={{fontSize: 48}}>
-            {track.name}
-        </Text>
+       <>
+
+           <Text style={{fontSize: 48}}>
+               {track.name}
+           </Text>
+
+           <MapView
+            initialRegion={{
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+                ...initialCoords
+            }}
+            style={styles.map}
+           >
+
+               <Polyline
+                   coordinates={track.locations.map(loc => loc.coords)}
+               />
+
+           </MapView>
+
+       </>
     );
 
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    map: {
+        height: 300
+    }
+});
 
 export default TrackDetailScreen;
