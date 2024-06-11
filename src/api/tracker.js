0dab6e26256a58cbc9ef7fd55fc694d5e5095e90
adelaf1999 <74821581+adelaf1999@users.noreply.github.com>
2024-06-11 16:46:17 -0400

    268 automatic authentication

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from "axios";
+import * as SecureStore from 'expo-secure-store';
 
-export default axios.create({
-    baseURL: 'https://7799-184-147-187-115.ngrok-free.app'
+const instance =  axios.create({
+    baseURL: 'https://2dce-184-146-214-137.ngrok-free.app'
 });
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await SecureStore.getItemAsync('token');
+        if(token){
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config;
+    },
+    (err) => {
+        return Promise.reject(err);
+    }
+);
+
+export default instance;
--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -1,4 +1,5 @@
 import createDataContext from "./createDataContext";
+import trackerApi from "../api/tracker";
 
 const INITIAL_STATE = {
 };
